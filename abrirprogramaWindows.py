b578bcd7967095729f165c0133fd78e3e6722097
GustavoGuimaraes01 <gustavodossantos111@hotmail.com>
2024-08-05 17:10:34 -0300

    V1

--- abrirprogramaWindows.py
+++ abrirprogramaWindows.py
@@ -1,8 +1,10 @@
 import os
 
-opcao = input("Digite 1 - Paint ou 2 - Calculadora")
+opcao = input("Digite\n(1) - Paint\n(2) - Calculadora\n(3) - Bloco de Notas\n")
 
 if opcao == "1":
     os.system("mspaint")
 if opcao == "2":
-    os.system("mscalculator")
+    os.system("calc")
+if opcao == "3":
+    os.system("notepad")
